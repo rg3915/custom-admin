d6f777783e337f07ffa3045d34ab650c2782c279
rg3915 <rg3915@yahoo.com.br>
2016-02-22 18:07:46 -0300

    Custom Login final

--- myproject/settings.py
+++ myproject/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
