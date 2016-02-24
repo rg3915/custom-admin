a73874d85bd20678380fe661aaaa608339c291e2
rg3915 <rg3915@yahoo.com.br>
2016-02-24 16:48:21 -0300

    Footer in all pages

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
+from myproject.core import views as c
 
 urlpatterns = [
+    url(r'^$', c.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
